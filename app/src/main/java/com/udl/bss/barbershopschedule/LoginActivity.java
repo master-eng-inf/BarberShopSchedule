31db24d96c8c9eba0d2e5cc40dc28954136b0286
gerard87 <gerar.f87@gmail.com>
2017-10-25 22:17:31 +0200

    Add a button to enter the home page

--- app/src/main/java/com/udl/bss/barbershopschedule/LoginActivity.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.udl.bss.barbershopschedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login_btn = (Button) findViewById(R.id.login_button);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
