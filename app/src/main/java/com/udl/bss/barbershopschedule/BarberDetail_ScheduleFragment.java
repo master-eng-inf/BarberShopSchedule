1eed63f593e56d7e00e0647e9939b2ba71c0b0ba
Alex Ghenghiu <alexgg94@hotmail.com>
2017-11-07 17:30:19 +0100

    Barber service prices activity added

--- app/src/main/java/com/udl/bss/barbershopschedule/BarberDetail_ScheduleFragment.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/BarberDetail_ScheduleFragment.java
@@ -1,11 +1,15 @@
 package com.udl.bss.barbershopschedule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.udl.bss.barbershopschedule.domain.Barber;
 
@@ -33,5 +37,23 @@ public class BarberDetail_ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_barber_detail__schedule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CalendarView calendar = (CalendarView) view.findViewById(R.id.client_schedule);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                Intent intent = new Intent();
+                intent.putExtra("","");
+
+                Toast.makeText(getActivity(), dayOfMonth+"-"+month+"-"+year, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
