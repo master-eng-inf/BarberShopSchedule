a96d8cc5e6e0c6c1feba95645896a86c701be7d8
julio77nz <julioqr94@gmail.com>
2017-11-08 21:13:31 +0100

    Barber Calendar (+ activity and layout for a concrete day)

--- app/src/main/java/com/udl/bss/barbershopschedule/BarberScheduleActivity.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/BarberScheduleActivity.java
@@ -1,26 +1,19 @@
 package com.udl.bss.barbershopschedule;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+
+import com.udl.bss.barbershopschedule.domain.Barber;
 
 public class BarberScheduleActivity extends AppCompatActivity {
+
+    private static final String BARBER_SHOP = "barber_shop";
+    private Barber barber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber_schedule);
-
-        Button btn_back = (Button) findViewById(R.id.btn_back);
-
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BarberHomeActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
