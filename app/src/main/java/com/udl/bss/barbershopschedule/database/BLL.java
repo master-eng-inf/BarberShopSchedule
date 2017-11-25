d7eb31f35e9eb5ee720febe61bf3382ad41ed57a
Alex Ghenghiu <alexgg94@hotmail.com>
2017-11-25 18:03:43 +0100

    Basic Business Logical Layer added

--- app/src/main/java/com/udl/bss/barbershopschedule/database/BLL.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/database/BLL.java
@@ -1,6 +1,29 @@
 package com.udl.bss.barbershopschedule.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+
+import com.udl.bss.barbershopschedule.R;
+import com.udl.bss.barbershopschedule.database.BE.AppointmentContract;
+import com.udl.bss.barbershopschedule.database.BE.BarberShopContract;
+import com.udl.bss.barbershopschedule.database.BE.PromotionContract;
+import com.udl.bss.barbershopschedule.database.BE.ReviewContract;
+import com.udl.bss.barbershopschedule.database.BE.ScheduleContract;
+import com.udl.bss.barbershopschedule.database.BE.ServiceContract;
+import com.udl.bss.barbershopschedule.database.BE.SpecialDayContract;
+import com.udl.bss.barbershopschedule.domain.Appointment;
+import com.udl.bss.barbershopschedule.domain.Barber;
+import com.udl.bss.barbershopschedule.domain.BarberService;
+import com.udl.bss.barbershopschedule.domain.Promotion;
+import com.udl.bss.barbershopschedule.domain.Review;
+import com.udl.bss.barbershopschedule.domain.Schedule;
+import com.udl.bss.barbershopschedule.domain.SpecialDay;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Alex on 24/11/2017.
@@ -13,5 +36,91 @@ public class BLL {
         this.dal_instance = DAL.GetInstance(context);
     }
 
+    public ArrayList<Barber> Get_BarberShopList() {
+        return this.dal_instance.Get_BarberShopList();
+    }
 
+    public Barber Get_BarberShop(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShop(barber_shop_id);
+    }
+
+    public void Insert_BarberShop(Barber new_barber_shop) {
+        this.dal_instance.Insert_BarberShop(new_barber_shop);
+    }
+
+    public void Delete_AllBarberShops() {
+        this.dal_instance.Delete_AllBarberShops();
+    }
+
+    public ArrayList<Review> Get_BarberShopReviews(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShopReviews(barber_shop_id);
+    }
+
+    public void Insert_Reviews(ArrayList<Review> reviews) {
+        this.Insert_Reviews(reviews);
+    }
+
+    public void Delete_Reviews() {
+        this.dal_instance.Delete_Reviews();
+    }
+
+    public ArrayList<BarberService> Get_BarberShopServices(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShopServices(barber_shop_id);
+    }
+
+    public void Insert_Services(ArrayList<BarberService> services) {
+        this.dal_instance.Insert_Services(services);
+    }
+
+    public void Delete_Services() {
+        this.dal_instance.Delete_Services();
+    }
+
+    public ArrayList<Appointment> Get_BarberShopAppointments(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShopAppointments(barber_shop_id);
+    }
+
+    public void Insert_Appointments(ArrayList<Appointment> appointments) {
+        this.dal_instance.Insert_Appointments(appointments);
+    }
+
+    public void Delete_Appointments() {
+        this.dal_instance.Delete_Appointments();
+    }
+
+    public ArrayList<Promotion> Get_BarberShopPromotions(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShopPromotions(barber_shop_id);
+    }
+
+    public void Insert_Promotions(ArrayList<Promotion> promotions) {
+        this.dal_instance.Insert_Promotions(promotions);
+    }
+
+    public void Delete_Promotions() {
+        this.dal_instance.Delete_Promotions();
+    }
+
+    public ArrayList<SpecialDay> Get_BarberShopSpecialDays(int barber_shop_id) {
+       return this.dal_instance.Get_BarberShopSpecialDays(barber_shop_id);
+    }
+
+    public void Insert_SpecialDays(ArrayList<SpecialDay> special_days) {
+        this.dal_instance.Insert_SpecialDays(special_days);
+    }
+
+    public void Delete_SpecialDays() {
+        this.dal_instance.Delete_SpecialDays();
+    }
+
+    public ArrayList<Schedule> Get_BarberShopSchedules(int barber_shop_id) {
+        return this.dal_instance.Get_BarberShopSchedules(barber_shop_id);
+    }
+
+    public void Insert_Schelues(ArrayList<Schedule> schedules) {
+        this.dal_instance.Insert_Schelues(schedules);
+    }
+
+    public void Delete_Schedules() {
+        this.dal_instance.Delete_Schedules();
+    }
 }
