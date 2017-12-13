7ca490568d2578c4eeebc9c38a345faf85783699
julio77nz <julioqr94@gmail.com>
2017-12-13 05:18:01 +0100

    Initial implementation of service creation

--- app/src/main/java/com/udl/bss/barbershopschedule/BarberNewServiceActivity.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/BarberNewServiceActivity.java
@@ -1,24 +1,45 @@
 package com.udl.bss.barbershopschedule;
 
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.udl.bss.barbershopschedule.database.BLL;
+import com.udl.bss.barbershopschedule.domain.BarberService;
+
 
 public class BarberNewServiceActivity extends AppCompatActivity {
+
+    private Activity activity;
+    private EditText new_service_name, new_service_price, new_service_duration;
+    private String new_service_name_var;
+    private double new_service_price_var;
+    private double new_service_duration_var;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber_new_service);
 
+        new_service_name = findViewById(R.id.new_service_name);
+        new_service_price = findViewById(R.id.new_service_price);
+        new_service_duration = findViewById(R.id.new_service_duration);
+
+
         Button btn_create = (Button) findViewById(R.id.btn_create);
 
         btn_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                insertInDB();
             }
         });
 
@@ -31,4 +52,38 @@ public class BarberNewServiceActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void insertInDB () {
+
+        if (creationCheck()) {
+
+            new_service_name_var = new_service_name.getText().toString();
+            new_service_price_var = Double.parseDouble(new_service_price.getText().toString());
+            new_service_duration_var = Double.parseDouble(new_service_duration.getText().toString());
+
+            BLL instance = new BLL(activity);
+
+            BarberService new_service = new BarberService(-1,0,new_service_name_var,new_service_price_var,new_service_duration_var);
+
+            instance.Insert_Service(new_service);
+
+            Toast.makeText(getApplicationContext(), "Your service was created succesfully", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(activity, HomeActivity.class);
+            intent.putExtra("barber1", "barber1");
+            activity.startActivity(intent);
+
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.field_error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean creationCheck () {
+
+        return new_service_name != null && !new_service_name.getText().toString().equals("")
+                && new_service_price != null && !new_service_price.getText().toString().equals("")
+                && new_service_duration != null && !new_service_duration.getText().toString().equals("");
+
+    }
+
 }
