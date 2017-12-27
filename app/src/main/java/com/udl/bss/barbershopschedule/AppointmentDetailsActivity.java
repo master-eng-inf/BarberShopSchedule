1ed29abd71f5740ad94f809a502336252ba938c2
julio77nz <julioqr94@gmail.com>
2017-12-27 03:12:19 +0100

    Added delete button on appointment details.

--- app/src/main/java/com/udl/bss/barbershopschedule/AppointmentDetailsActivity.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/AppointmentDetailsActivity.java
@@ -1,10 +1,12 @@
 package com.udl.bss.barbershopschedule;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.udl.bss.barbershopschedule.database.BLL;
 import com.udl.bss.barbershopschedule.domain.Appointment;
@@ -23,7 +26,7 @@ import java.util.Locale;
 
 public class AppointmentDetailsActivity extends AppCompatActivity {
 
-    FloatingActionButton fab;
+    FloatingActionButton delete_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +35,30 @@ public class AppointmentDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        delete_btn = (FloatingActionButton) findViewById(R.id.delete_btn);
+        delete_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog alert = new AlertDialog.Builder(AppointmentDetailsActivity.this).create();
+                alert.setTitle(getString(R.string.delete_appointment_dialog_title));
+                alert.setMessage(getString(R.string.delete_appointment_dialog));
+                alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.accept_button), new DialogInterface.OnClickListener() {
+                @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.appointment_deleted), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alert.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancel_button), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alert.show();
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //       .setAction("Action", null).show();
             }
         });
 
@@ -45,7 +66,7 @@ public class AppointmentDetailsActivity extends AppCompatActivity {
             Fade fade = new Fade();
             fade.excludeTarget(android.R.id.statusBarBackground, true);
             fade.excludeTarget(android.R.id.navigationBarBackground, true);
-            fade.excludeTarget(R.id.fab, true);
+            fade.excludeTarget(R.id.delete_btn, true);
 
             getWindow().setEnterTransition(fade);
             getWindow().setExitTransition(fade);
@@ -57,7 +78,7 @@ public class AppointmentDetailsActivity extends AppCompatActivity {
         anim.setFillEnabled(true);
         anim.setDuration(300);
         anim.setInterpolator(new OvershootInterpolator());
-        fab.startAnimation(anim);
+        delete_btn.startAnimation(anim);
 
         TextView tv_date = findViewById(R.id.date_detail);
         TextView tv_service = findViewById(R.id.service_detail);
