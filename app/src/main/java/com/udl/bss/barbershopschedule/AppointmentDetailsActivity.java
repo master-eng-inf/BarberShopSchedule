7561f5c16f554fc2d649bbccaa163b4984b5a5f5
Gerard <gerar.f87@gmail.com>
2018-01-07 20:44:38 +0100

    Get data from server on appointment details activity

--- app/src/main/java/com/udl/bss/barbershopschedule/AppointmentDetailsActivity.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/AppointmentDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.udl.bss.barbershopschedule;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -15,9 +17,14 @@ import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
 import com.udl.bss.barbershopschedule.domain.Appointment;
 import com.udl.bss.barbershopschedule.domain.Barber;
 import com.udl.bss.barbershopschedule.domain.BarberService;
+import com.udl.bss.barbershopschedule.domain.Client;
+import com.udl.bss.barbershopschedule.serverCommunication.APIController;
 
 
 public class AppointmentDetailsActivity extends AppCompatActivity {
@@ -30,6 +37,20 @@ public class AppointmentDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_appointment_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences mPrefs = getSharedPreferences("USER", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("user", "");
+        String mode = mPrefs.getString("mode", "");
+
+        String token;
+        if (mode.equals("Barber")) {
+            Barber barber = gson.fromJson(json, Barber.class);
+            token = barber.getToken();
+        } else {
+            Client client = gson.fromJson(json, Client.class);
+            token = client.getToken();
+        }
 
         delete_btn = findViewById(R.id.delete_btn);
         delete_btn.setOnClickListener(new View.OnClickListener() {
@@ -75,19 +96,29 @@ public class AppointmentDetailsActivity extends AppCompatActivity {
         delete_btn.startAnimation(anim);
 
         TextView tv_date = findViewById(R.id.date_detail);
-        TextView tv_service = findViewById(R.id.service_detail);
-        CollapsingToolbarLayout ctl = findViewById(R.id.toolbar_layout);
+        final TextView tv_service = findViewById(R.id.service_detail);
+        final CollapsingToolbarLayout ctl = findViewById(R.id.toolbar_layout);
 
         Appointment a = getIntent().getParcelableExtra("appointment");
 
-        /*TODO*/
-        //BLL instance = new BLL(this);
-        //Barber barber = instance.Get_BarberShop(a.getBarber_shop_id());
-        //BarberService service = instance.Get_BarberShopService(a.getService_id());
+        APIController.getInstance().getBarberById(token, String.valueOf(a.getBarber_shop_id())).addOnCompleteListener(new OnCompleteListener<Barber>() {
+            @Override
+            public void onComplete(@NonNull Task<Barber> task) {
+                Barber barber = task.getResult();
+                ctl.setTitle(barber.getName());
+            }
+        });
+
+        APIController.getInstance().getServiceById(token, String.valueOf(a.getService_id())).addOnCompleteListener(new OnCompleteListener<BarberService>() {
+            @Override
+            public void onComplete(@NonNull Task<BarberService> task) {
+                BarberService service = task.getResult();
+                tv_service.setText(service.getName());
+            }
+        });
 
         //TODO format date
-        //ctl.setTitle(barber.getName());
         tv_date.setText(a.getDate());
-        //tv_service.setText(service.getName());
+
     }
 }
