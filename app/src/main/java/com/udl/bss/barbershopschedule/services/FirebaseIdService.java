f5cbf573dbd45361520b5234c67caf9d95a09b0f
Gerard <gerar.f87@gmail.com>
2018-01-11 17:52:20 +0100

    Refresh firebase token and send notification when message is received

--- app/src/main/java/com/udl/bss/barbershopschedule/services/FirebaseIdService.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/services/FirebaseIdService.java
@@ -1,15 +1,26 @@
 package com.udl.bss.barbershopschedule.services;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 
 public class FirebaseIdService extends FirebaseInstanceIdService {
 
+    private final static String TAG = "FCM";
+
     public FirebaseIdService() { }
 
     @Override
     public void onTokenRefresh() {
-        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
 
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
     }
 }
--- app/src/main/java/com/udl/bss/barbershopschedule/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/udl/bss/barbershopschedule/services/MyFirebaseMessagingService.java
@@ -1,9 +1,20 @@
 package com.udl.bss.barbershopschedule.services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.udl.bss.barbershopschedule.HomeActivity;
+import com.udl.bss.barbershopschedule.R;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -14,7 +25,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
+
+        String message = "";
 
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
@@ -23,7 +35,42 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
 
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            message = remoteMessage.getNotification().getBody();
+            Log.d(TAG, "Message Notification Body: " + message);
         }
+
+        sendNotification(message);
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+                        .setSmallIcon(R.mipmap.logo_icon)
+                        .setContentTitle("Barber Shop Schedule Message")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (notificationManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(getString(R.string.default_notification_channel_id),
+                        "Default chanel",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(0, notificationBuilder.build());
+        }
+
+
     }
 }
